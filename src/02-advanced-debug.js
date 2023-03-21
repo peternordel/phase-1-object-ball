cca0191759054f5bb6e77000348b26c0f686c0e5
Peter Nordel <peternordel@gmail.com>
2023-03-20 21:51:15 -0600

    complete lab

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -9,17 +9,9 @@ function goodPractices() {
     // use the debugger to find out!
     debugger
     let teamObj = game[gameKey]
-    for (let teamKey in teamObj) {
-      // are you ABSOLUTELY SURE what 'teamKey' is?
-      // use debugger to find out!
+    let data = teamObj.players
+    for (let key in data) {
       debugger
-
-      // what is 'data' at each loop through out this block?
-      // when will the following line of code work and when will it break?
-      let data = teamObj.player
-      for (let key in data) {
-        debugger
-      }
     }
   }
 }
